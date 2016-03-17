639771c52a0bdbe3d8544c9e2556520d000c3bb9
Verónica <verosgfc11@gmail.com>
2016-03-17 11:04:45 +0100

    Creamos tantas bolas como nos indique el usuario

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,5 @@
 import java.awt.Color;
-
+import java.util.ArrayList;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,19 +11,20 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private ArrayList<BouncingBall> bolas;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        bolas = new ArrayList<>();
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numeroDeBolas)
     {
         int ground = 400;   // position of the ground line
 
@@ -32,22 +33,22 @@ public class BallDemo
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
+        for(int i = 0; i < numeroDeBolas; i++){
+            BouncingBall ball = new BouncingBall(50 + (20*i), 50, 16, Color.BLUE, ground, myCanvas);
+            ball.draw();
+            bolas.add(ball);
+        }
 
         // make them bounce
         boolean finished =  false;
         while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
-            }
+            myCanvas.wait(50);  // small delay
+            for(int i = 0; i < numeroDeBolas; i++){
+                bolas.get(i).move();
+                if(bolas.get(i).getXPosition() >= 550) {
+                    finished = true;
+                }
+            }           
         }
     }
 }
